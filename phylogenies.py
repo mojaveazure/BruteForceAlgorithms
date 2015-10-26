42b1a44e195c541fa51b1dca674315fb38c2aa1f
Paul Hoffman <hoff0792@umn.edu>
2015-10-26 11:43:43 -0500

    new script

---
+++ phylogenies.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#   Looking at solving phylogenetic trees using brute force algorithms
+#       First, our sequences
+A = 'GATCCATGA'
+B = 'GATCTATGC'
+C = 'GTCCCATTT'
+D = 'AATCCGATC'
+E = 'TCTCGATAG'
+
